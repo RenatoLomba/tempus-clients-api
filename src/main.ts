21af366de0583b9ea9e6858b1807339d3b6ca516
RenatoLomba <63821245+RenatoLomba@users.noreply.github.com>
2021-07-15 19:53:06 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { PrismaService } from './prisma/prisma.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
   const prismaService: PrismaService = app.get(PrismaService);
   prismaService.enableShutdownHooks(app);
 
